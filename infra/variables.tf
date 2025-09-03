79f592cba2e8d205675e9f2ff1137267d59a479b
Sandeep Kaza <47395969+sandeepkaza@users.noreply.github.com>
2025-09-02 22:29:19 -0700

    Configure ARM environment variables for Terraform authentication

---
+++ infra/variables.tf
@@ -0,0 +1,32 @@
+variable "subscription_id" {
+  description = "Azure Subscription ID"
+  type        = string
+}
+
+variable "client_id" {
+  description = "Azure Service Principal Client ID"
+  type        = string
+}
+
+variable "client_secret" {
+  description = "Azure Service Principal Client Secret"
+  type        = string
+  sensitive   = true
+}
+
+variable "tenant_id" {
+  description = "Azure Tenant ID"
+  type        = string
+}
+
+variable "environment_name" {
+  description = "Environment name"
+  type        = string
+  default     = "dev"
+}
+
+variable "location" {
+  description = "Azure region"
+  type        = string
+  default     = "eastus"
+}
